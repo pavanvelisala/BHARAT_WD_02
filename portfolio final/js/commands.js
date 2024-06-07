76760904a1dfc0153332c241d185e20ef37a6f7b
pavanvelisala <103478304+pavanvelisala@users.noreply.github.com>
2024-06-07 17:56:50 +0530

    Add files via upload

---
+++ portfolio final/js/commands.js
@@ -0,0 +1,88 @@
+// These are the variable for the direct links
+var linkedin = "https://www.linkedin.com/in/pavanvelisala/";
+var instagram = "https://www.instagram.com/velisala.04/";
+var email = 'mailto:pavanvalisala@gmail.com';
+var resume = 'resume_colour.pdf';
+
+// This content is about the "WHOIS"
+whois = [
+  "<br>",
+  "- A Undergraduate student studying Btech 3rd year in Gitam University",
+  "<br>",
+  "- Skills:",
+  "<ul>",
+  "<li>Web Application Pentesting</li>",
+  "<li>Malware Analysis</li>",
+  "<li>Networking</li>",
+  "<li>Buffer Overflows</li>",
+  "<li>Bash</li>",
+  "<li>Threat monitoring and log analysis</li>",
+  "</ul>",
+  "<br>",
+  "- An Engaged Member and Volunteer at a cybersecuiryt student club At Gitam University",
+  "<br>",
+  "- He shares his insights and knowledge through blogs,writeups, focusing on security findings, vulnerability identification methodologies, and Malware",
+  "<br>"
+];;
+
+experience = [
+  "<br>",
+  "Cloud Security Engineer",
+  "<ul>",
+  "<li>VTfoundation</li>",
+  "</ul>",
+  "<br>",
+  "Vice President",
+  "<ul>",
+  "<li>Cysec (Cybersecurity Student Club at Gitam University)</li>",
+  "</ul>",
+  "<br>",
+];
+
+certificates = [
+  "<br>",
+  '1. Certified Ethical Hacker (CEH)',
+  '2. Oracle Cloud Infrastructure Associate',
+  '3. Certified Network Security Practitioner',
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/perplex007' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/perplex_007' + '</a>',
+  'mail           <a href="' + email + '">santoshvardan007@gmail.com' + "</a>",
+  "<br>"
+];
+
+blogs = [
+  "<br>",
+  '1. Cyber Stuff Blog <a href="https://cyberstuffa1.blogspot.com/" target="_blank">cyberstuffa1.blogspot.com' + "</a>",
+  "<br>"
+];
+
+banner = [
+  "<br>",
+  "+-+-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+-+",
+  "|P|A|V|A|N| |V|E|L|I|S|A|L|A|", 
+  "+-+-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+-+",
+   "<span class=\"color2\">For a 📃 of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  "<span class=\"color2\">Take a 😒 at My Resume to get some 💡 about me with</span> <a href=\"Daniel_Thotapalli.pdf\" class=\"command\">'resume'</a><span class=\"color2\">.</span>",
+  "<br>",
+  "<span class=\"color2\">Note: Don't Hack me with</span> <span class=\"command\">'SUDO'</span><span class=\"color2\">.</span>",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          About Me',
+  '<span class="command">social</span>         My Social platforms',
+  '<span class="command">experience</span>     My Experience',
+  '<span class="command">certificates</span>   My Certificates',
+  '<span class="command">clear</span>          Clear terminal',
+  "<br>",
+];
+
+// Remove unnecessary content
+projects = [];
+secret = [];
